8478a018c25db88a795246b68ac5c5cb370d4c95
Mikkel Hoegh <mikkel@hoegh.org>
2012-03-08 23:04:07 +0100

    Using configuration for HTTP port.

--- app.js
+++ app.js
@@ -1,13 +1,27 @@
+"use strict";
+
 var flatiron = require('flatiron'),
     path = require('path'),
     app = flatiron.app;
 
 app.config.file({ file: path.join(__dirname, 'config', 'config.json') });
 
+// Default options defined here.
+// These can be overridden by creating a config/config.json file with
+// the same format.
+app.config.defaults({
+  "http": {
+    "server": {
+      "port": 7000
+    }
+  }
+});
+
 app.use(flatiron.plugins.http);
 
 app.router.get('/', function () {
-  this.res.json({ 'hello': 'world' })
+  this.res.json({ 'hello': 'world' });
 });
 
-app.start(3000);
+app.start(app.config.get('http:server:port'));
+app.log.info('Razdraz server listening on port ' + app.config.get('http:server:port'));
